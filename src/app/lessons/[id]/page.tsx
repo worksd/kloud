2c63329399eee02208fdd0dde7dcc4b34a67206b
koos <rnrudxo2872@naver.com>
2024-11-24 14:51:43 +0900

    수업 결제상세 페이지로 이동할 수업 상세 페이지 더미 작성

--- src/app/lessons/[id]/page.tsx
+++ src/app/lessons/[id]/page.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
+import { useMemo } from "react";
 
+export default function LessonDetail({ params }: { params: { id: string } }) {
+    const isPaymentButtonVisible = useMemo(() => true, []);
 
-export default function LessonDetail(props: any) {
-
-
-  console.log(props);
-
-  return (
-    <>
-      {/*<div>Hello Lesson Detail {router..id} </div>*/}
-    </>
-  );
+    return (
+        <div className="w-screen h-screen bg-white">
+            {isPaymentButtonVisible && (
+                <Link
+                    href={`/lessons/${params.id}/payment`}
+                    className="flex justify-center items-center w-[342px] h-14 relative px-4 rounded-lg bg-black">
+                    <p className="flex-grow-0 flex-shrink-0 text-base font-medium text-center text-white">30,000원 결제하기</p>
+                </Link>
+            )}
+        </div>
+    );
 }
---
+++ src/app/lessons/[id]/payment/page.tsx
@@ -0,0 +1,5 @@
+export default function LessonPayment(props: any) {
+    return <div className="w-screen h-screen bg-white">
+        
+    </div>;
+}
