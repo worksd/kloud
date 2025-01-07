78ba808c0c947c9074caaaa5e899591256a02bc1
이동호 <dongho.lee11@cj.net>
2025-01-07 18:23:33 +0900

    에러 로깅 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div>
-      <h2 className="text-black">아직 개발중입니다~</h2>
+      <h2 className="text-black">아직 개발중입니다~ {process.env.GUINNESS_API_SERVER}가 서버입니다</h2>
     </div>
   )
 }
