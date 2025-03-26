bff95125962d589bed8b0d1dcc14a641cbcbd94d
이동호 <dongho.lee11@cj.net>
2025-03-26 23:31:13 +0900

    type id paymentId로부터 받게 수정

--- src/app/payment-redirect/page.tsx
+++ src/app/payment-redirect/page.tsx
@@ -2,6 +2,7 @@ import { createTicketAction } from "@/app/lessons/[id]/payment/create.ticket.act
 import { KloudScreen } from "@/shared/kloud.screen";
 import { createPassAction } from "@/app/passPlans/[id]/payment/create.pass.action";
 import { redirect } from "next/navigation";
+import { extractNumber } from "@/utils";
 
 export default async function PaymentRedirectPage({searchParams}:
                                                     {
@@ -14,18 +15,18 @@ export default async function PaymentRedirectPage({searchParams}:
                                                     }
 ) {
 
-  const {type, paymentId, id, message} = await searchParams;
-
+  const {paymentId, message} = await searchParams;
+  const type = paymentId.split('-')[0]
+  const id = paymentId.split('-')[1]
   if (message) {
     return <div>{message}</div>
   } else {
-    if (type == 'lesson') {
-      const res = await createTicketAction({paymentId: paymentId, lessonId: id, status: 'Paid'});
+    if (type == 'LT') {
+      const res = await createTicketAction({paymentId: paymentId, lessonId: extractNumber(id), status: 'Paid'});
       const route = 'id' in res ? KloudScreen.TicketDetail(res.id, true) : null
       if (route) redirect(route)
-
-    } else if (type == 'passPlan') {
-      const res = await createPassAction({paymentId: paymentId, passPlanId: id, status: 'Active'});
+    } else if (type == 'LP') {
+      const res = await createPassAction({paymentId: paymentId, passPlanId: extractNumber(id), status: 'Active'});
       const route = 'id' in res ? KloudScreen.PassPaymentComplete(res.id) : null
       if (route) redirect(route)
     }
