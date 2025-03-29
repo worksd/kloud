b7d4b870324ed99ef50005eb4d247561d4610cbd
이동호 <dongho.lee11@cj.net>
2025-03-29 17:27:36 +0900

    바텀메뉴 중국어로 나오게 수정

--- src/utils/bottom.menu.fetch.action.ts
+++ src/utils/bottom.menu.fetch.action.ts
@@ -6,5 +6,6 @@ export async function getBottomMenuList() {
   const locale = await getLocale();
   if (locale == "ko") return JSON.parse(process.env.NEXT_PUBLIC_BOTTOM_MENU_LIST || "[]");
   else if (locale == 'jp') return JSON.parse(process.env.NEXT_PUBLIC_BOTTOM_MENU_LIST_JP || "[]");
+  else if (locale == 'zh') return JSON.parse(process.env.NEXT_PUBLIC_BOTTOM_MENU_LIST_ZH || "[]")
   else return JSON.parse(process.env.NEXT_PUBLIC_BOTTOM_MENU_LIST_EN || "[]");
 }
