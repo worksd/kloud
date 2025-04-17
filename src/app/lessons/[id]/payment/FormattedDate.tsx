db229b3d128397f758742018e89c3ca32ddcfabe
이동호 <dongho.lee11@cj.net>
2025-04-17 14:46:20 +0900

    결제 페이지에서 요일이 key로 나오는 이슈 수정

--- src/app/lessons/[id]/payment/FormattedDate.tsx
+++ src/app/lessons/[id]/payment/FormattedDate.tsx
@@ -1,9 +1,10 @@
 'use server'
 import { formatDateTime } from "@/utils/date.format";
+import { translate } from "@/utils/translate";
 
 export const FormattedDate = async ({startTime} : { startTime: string}) => {
   const formattedTime = await formatDateTime(startTime ?? '')
   return <p className="text-[#86898C] text-[14px] font-medium">
-    {formattedTime.date} ({formattedTime.dayOfWeek}) {formattedTime.time}
+    {formattedTime.date} ({await translate(formattedTime.dayOfWeek)}) {formattedTime.time}
   </p>
 }
