f74bf0b71ebf4791fdd72e91c985f8faa75cb208
이동호 <dongho.lee11@cj.net>
2025-05-05 14:07:28 +0900

    패스권 결제 페이지에서 Type에 따라서 메타 추가로 보여주기

--- src/app/passPlans/[id]/payment/CurrentPassPlan.tsx
+++ src/app/passPlans/[id]/payment/CurrentPassPlan.tsx
@@ -3,21 +3,27 @@ import TicketIcon from "../../../../../public/assets/ic_ticket.svg";
 import React from "react";
 import { GetPassPlanResponse } from "@/app/endpoint/pass.endpoint";
 
-export const CurrentPassPlan = ({passPlan} : {passPlan?: GetPassPlanResponse}) => {
+export const CurrentPassPlan = ({passPlan}: { passPlan?: GetPassPlanResponse }) => {
 
   return (
     <div className={"px-6"}>
       <div className={"text-[16px] text-black font-medium"}>
         {passPlan?.studio?.name}
       </div>
-      <div className={"flex flex-row justify-between items-center"}>
-        <div className={"flex flex-row items-center space-x-3"}>
+      <div className="flex flex-row justify-between items-center">
+        <div className="flex flex-row items-center space-x-3 my-3">
           <div
-            className="w-[32px] h-[32px] rounded-full overflow-hidden flex-shrink-0 bg-[#F3F3F4] flex items-center justify-center my-5">
+            className="w-[32px] h-[32px] rounded-full overflow-hidden flex-shrink-0 bg-[#F3F3F4] flex items-center justify-center">
             <TicketIcon/>
           </div>
-          <div className={"text-[16px] text-black font-medium"}>
-            {passPlan?.name}
+          <div className="flex flex-col justify-center">
+            <div className="text-[16px] text-black font-medium">
+              {passPlan?.name}
+            </div>
+            <div className="text-[12px] text-[#A4A4A4] font-paperlogy">
+              {passPlan?.type === 'Unlimited' && <div>모든 클래스 무제한 이용 가능</div>}
+              {passPlan?.type === 'Count' && <div>클래스 {passPlan.usageLimit}회 이용 가능</div>}
+            </div>
           </div>
         </div>
       </div>
