ccf9355bf8cc83abf28d62b14a4824ed4bedc5fb
이동호 <dongho.lee11@cj.net>
2025-05-07 12:16:23 +0900

    당일 팝업 다이얼로그 문구 수정

--- src/app/studios/TodayLessonWrapper.tsx
+++ src/app/studios/TodayLessonWrapper.tsx
@@ -8,9 +8,9 @@ export const TodayLessonWrapper = ({ticket}: { ticket: TicketResponse }) => {
   //TODO: 테스트 데이터 지우기
   useEffect(() => {
     const dialogInfo = {
-      id: '',
+      id: 'TodayClass',
       route: `/tickets/${ticket.id}`,
-      hideForeverMessage: '',
+      hideForeverMessage: '오늘 하루 보지 않기',
       imageUrl: ticket.lesson?.thumbnailUrl,
       imageRatio: 0.7,
       ctaButtonText: ticket.lesson?.title + ' 바로가기',
